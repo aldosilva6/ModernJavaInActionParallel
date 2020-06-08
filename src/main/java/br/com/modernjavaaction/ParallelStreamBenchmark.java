a90fcafb380bcf1ad812275e06db5179fd292f06
ainacio <ainacio@close-upinternational.com.br>
2020-06-08 16:14:07 -0300

    Adding some methods to test performance of parallelism

--- src/main/java/br/com/modernjavaaction/ParallelStreamBenchmark.java
+++ src/main/java/br/com/modernjavaaction/ParallelStreamBenchmark.java
@@ -9,18 +9,29 @@ import org.openjdk.jmh.annotations.Fork;
 import org.openjdk.jmh.annotations.Level;
 import org.openjdk.jmh.annotations.Mode;
 import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.annotations.TearDown;
 
 @BenchmarkMode(Mode.AverageTime)
 @OutputTimeUnit(TimeUnit.MILLISECONDS)
 @Fork(value = 2, jvmArgs = { "-Xms4G", "-Xmx4G" })
+@State(Scope.Benchmark)
 public class ParallelStreamBenchmark {
 
 	private static final long N = 10_000_000L;
 
 	@Benchmark
-	public long sequentialSum() {
-		return Stream.iterate(1L, i -> i + 1).limit(N).reduce(0L, Long::sum);
+	/*
+	 * public long sequentialSum() { return Stream.iterate(1L, i -> i +
+	 * 1).limit(N).reduce(0L, Long::sum); }
+	 */
+	/*
+	 * public long iterativeSum() { long result = 0; for (long i = 1L; i <= N; i++)
+	 * { result += i; } return result; }
+	 */
+	public long parallelSum() {
+		return Stream.iterate(1L, i -> i + 1).limit(N).parallel().reduce(0L, Long::sum);
 	}
 
 	@TearDown(Level.Invocation)
